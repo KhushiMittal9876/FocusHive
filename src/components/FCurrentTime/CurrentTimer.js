7f2c19a01ddb719bdf030dccc045511ca71fcb39
KhushiMittal <mittalkhushi9876@gmail.com>
2024-08-27 23:04:07 +0530

    draggable date and time

--- src/components/FCurrentTime/CurrentTimer.js
+++ src/components/FCurrentTime/CurrentTimer.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import "./CurrentTime.css"
 import { useState } from 'react'
+import { motion } from 'framer-motion';
 
 function CurrentTimer() {
   const date = new Date().getDate();
-  const month = new Date().getMonth();
+  const month = new Date().getMonth() + 1;
+  // getMonth() gives us previous month,Because getMonth() start from 0. You may want to have getMonth() + 1 to achieve what you want.
   const year = new Date().getFullYear();
 
   let time =  new Date().toLocaleTimeString();
@@ -17,10 +19,13 @@ function CurrentTimer() {
 setInterval(update, 1000);
 
   return (
-    <div className='currentDateTime'>
+    <motion.div className='currentDateTime' 
+    drag
+    dragConstraints={{ left:0, right: 900 ,top: 0,bottom :200 }}
+    dragElastic={2} >
     <span>{date}/{month}/{year}</span>
-      <h1>{ctime}</h1>
-    </div>
+    <h1>{ctime}</h1>
+    </motion.div>
   )
 }
 
